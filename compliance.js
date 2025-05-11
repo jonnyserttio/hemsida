7c06b4f9b18957449fbb1d31f2700447974ccf71
jonnyserttio <jonny.serttio@gmail.com>
2025-05-11 17:29:24 +0200

    Delete compliance.js

--- compliance.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const ComplianceSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  items: [String]
-});
-
-module.exports = mongoose.model('Compliance', ComplianceSchema);
